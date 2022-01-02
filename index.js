158101611f0d72dc8c58207865bd2afb59ba3dd8
Aaron Elias <aaronelias007@gmail.com>
2022-01-01 19:57:01 -0500

    commit tree c1b2914caee7bd424f543e956cdb05e899df3f3f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching (drivers, string) {
+    
+    const answer = drivers.filter(driversName => driversName.toLowerCase() === string.toLowerCase());
+    
+    return answer;
+}
+
+function fuzzyMatch (drivers, string) {
+    
+    const answer = drivers.filter(driversName => driversName.startsWith(string));
+
+    return answer;
+}
+
+function matchName (drivers, string) {
+
+    const answer = drivers.filter(drivers => drivers === string.length);
+
+    return answer;
+}
